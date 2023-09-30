1aa59c6aeeb20a08f4d7b0891aa97d6f6c622c96
Mila <m9234434@gmail.com>
2023-09-30 10:03:29 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,39 @@
+    return data
+    for item in data:
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+
+
+
+    main()
+    main()
+
+if __name__ == "__main__":
+def main():
+    for item in data:
+
+    data = generate_random_data()
+    return data
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+import random
+
             System.out.println("Random Number: " + item);
-        }
-}
-
-
-        }
-public class RandomDataGenerator {
-        int[] data = new int[10];
         for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
         for (int i = 0; i < 10; i++) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
     public static void main(String[] args) {
+        }
+        }
+
     }
